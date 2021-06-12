9bbca4514e6dc55476223b2045f400cd2be6c2bb
Jay Patel <pjay20301@gmail.com>
2021-06-12 23:26:01 +0530

    http server set

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+
 const app = express();
+dotenv.config({path: 'config.env'})
+const port = process.env.port
+
+app.use(morgan('tiny'));
 
 app.get('/', (req,res) => {
     res.send('crud application');
 });
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000')
+app.listen(port, () => {
+    console.log(`server is running on port https://localhost:${port}`)
 });
