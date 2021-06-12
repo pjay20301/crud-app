b32d3ce1c875bfb68698f5d50b17966363bdf6b6
Jay Patel <pjay20301@gmail.com>
2021-06-12 23:50:19 +0530

    modules imported

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 
 const app = express();
+
 dotenv.config({path: 'config.env'})
 const port = process.env.port
 
 app.use(morgan('tiny'));
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.set("view engine", "ejs")
+
+app.use(express.static('static'))
 
 app.get('/', (req,res) => {
     res.send('crud application');
