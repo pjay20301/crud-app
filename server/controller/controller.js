6d87eae86dce1d36261a19a23dfa0ab042313926
Jay Patel <pjay20301@gmail.com>
2021-06-13 13:36:45 +0530

    update user API checked

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,7 +2,7 @@ const { reset } = require('nodemon');
 var User = require('../model/model') 
 
 exports.create = (req,res) => {
-    
+
     if(!req.body) {
         res.status(400).send({message: "Content cannot be empty!"});
         return;
@@ -38,7 +38,29 @@ exports.find = (req,res) => {
 }
 
 exports.update = (req,res) => {
-    
+    if(!req.body) {
+        return res
+            .status(400)
+            .send({message: `Data to be updated cannot be empty`})
+    }
+
+    const id = req.params.id;
+
+    User.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+    .then(data=> {
+        if(!data) {
+            res.status(404).send({
+                message: `cannot update user with ${id}. Maybe uer not found`
+            }) 
+        } else {
+            res.send(data)
+        }
+    })
+    .catch(err=> {
+        res.status(500).send({
+            message: `Error update user information`
+        })
+    })
 }
 
 exports.delete = (req,res) => {
