c89efc9c3460ee25d2e9957e4393cbee15306674
Jay Patel <pjay20301@gmail.com>
2021-06-19 18:18:34 +0530

    routes updated

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const services = require('../services/render')
-const controller = require('../controller/controller')
+const author = require('../controller/author')
+const book = require('../controller/book')
 
-router.get('/', services.homeRoutes);
 
-router.get('/add-user', services.add_user);
+router.post('/api/authors', author.create);
 
-router.get('/update-user', services.updarte_user);
+router.get('/api/authors', author.find);
 
-router.post('/api/users', controller.create);
+router.put('/api/authors/:id', author.update);
 
-router.get('/api/users', controller.find);
+router.delete('/api/authors/:id', author.delete);
 
-router.put('/api/users/:id', controller.update);
+router.post('/api/books', book.create);
 
-router.delete('/api/users/:id', controller.delete);
+router.get('/api/books', book.find);
+
+router.put('/api/books/:id', book.update);
+
+router.delete('/api/books/:id', book.delete);
 
 module.exports = router
